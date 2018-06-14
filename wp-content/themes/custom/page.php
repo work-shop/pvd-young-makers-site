3dc3a73ef9526cca2540549f1b8098d2aaf2d700
Teddy Bradford <teddy@bearform.xyz>
2018-06-14 14:52:15 -0400

    Update section styles, add additional about and events to home page

--- wp-content/themes/custom/page.php
+++ wp-content/themes/custom/page.php
@@ -9,6 +9,13 @@ if (is_front_page()) {
 
 $context = Timber::get_context();
 $context['post'] = $post;
-$context['news'] = Timber::get_posts('post_type=news&numberposts=2');
+if (is_front_page()) {
+    $context['news'] = Timber::get_posts('post_type=news&numberposts=2');
+    $events = Timber::get_posts('post_type=events&numberposts=3');
+    foreach ($events as $event) {
+        $event->location_name = Timber::get_post('post_type=locations&p=' . $event->location[0])->post_title;
+    }
+    $context['events'] = $events;
+}
 
 Timber::render($templates, $context);
