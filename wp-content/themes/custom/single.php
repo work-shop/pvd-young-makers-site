f91fff51aa5938f3fd1b1fafc70432ee18ac1c0a
Teddy Bradford <teddy@bearform.xyz>
2018-06-21 16:27:58 -0400

    Add single event template

--- wp-content/themes/custom/single.php
+++ wp-content/themes/custom/single.php
@@ -12,4 +12,32 @@ if ($post_type === 'locations') {
     $context['tools'] = Timber::get_posts('post_type=tools&numberposts=4');
 }
 
+if ($post_type === 'events') {
+    $tools_args = array(
+        'post_type' => 'tools',
+        'meta_query' => array(
+            array(
+                'key'     => 'events',
+                'value'   => '"' . $post->id . '"',
+                'compare' => 'LIKE',
+            ),
+        ),
+    );
+    $tools = Timber::get_posts($tools_args);
+    $context['tools'] = $tools;
+
+    // Get array of badges from tools that will be at the event
+    $array_of_arrays_of_badge_ids = [];
+    foreach ($tools as $tool) {
+        array_push($array_of_arrays_of_badge_ids, $tool->badge);
+    }
+    $array_of_badge_ids = array_column($array_of_arrays_of_badge_ids, 0);
+    array_unique($array_of_badge_ids);
+    $badges_args = array(
+        'post_type' => 'badges',
+        'post__in' => $array_of_badge_ids
+    );
+    $context['badges'] = Timber::get_posts($badges_args);
+}
+
 Timber::render($templates, $context);
