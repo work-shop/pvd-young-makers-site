780d4de17b023e258dd1ea6e8c3e1707866181c9
Nic Schumann <nicolas_schumann@brown.edu>
2018-06-29 16:38:56 -0400

    [Livereload] added a guard to prevent livereload in production.

--- wp-content/themes/custom/scripts/modules/livereload-client.js
+++ wp-content/themes/custom/scripts/modules/livereload-client.js
@@ -1,8 +1,14 @@
 export function livereload() {
-  document.write(
-    '<script src="http://' +
-      (location.host || "localhost").split(":")[0] +
-      ':35729/livereload.js?snipver=1"></' +
-      "script>"
-  );
+
+    if ( window.location.host.indexOf('pvdyoungmakers.com') === -1 ) {
+      console.log( 'livereload enabled' );
+      document.write(
+        '<script src="http://' +
+          (location.host || "localhost").split(":")[0] +
+          ':35729/livereload.js?snipver=1"></' +
+          "script>"
+      );
+  } else {
+      console.log( 'livereload disabled' );
+  }
 }
