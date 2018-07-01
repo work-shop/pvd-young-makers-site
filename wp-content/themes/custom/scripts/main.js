c6a3fe8e03babde5be0262f2b19bdffd4052c891
rubonics <rubenrodriguez+github@gmail.com>
2018-07-01 12:44:55 -0400

    [theme js main] add map module

--- wp-content/themes/custom/scripts/main.js
+++ wp-content/themes/custom/scripts/main.js
@@ -1,6 +1,19 @@
 import { livereload } from "./modules/livereload-client.js";
 import { Calendar } from "./modules/calendar.js";
+import makeSlippyMaps from '@work-shop/map-module';
 
 livereload();
 
 const calendar = new Calendar();
+const maps = makeSlippyMaps( {
+  selector: '.ws-map',
+  map: {
+    mapTypeControl: false,
+    fullscreenControl: false,
+  },
+  marker: {
+    popup: {
+      pointer: '8px',
+    },
+  },
+} )
